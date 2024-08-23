018013eabafd76f36c2cd10ddf9b977821f0b6c8
Daniel Stéphane <mbappedaniel00@yahoo.fr>
2024-08-23 18:43:24 +0200

    Pages almost done

---
+++ app/(public)/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from 'expo-router';
+
+export default function PublicLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="sign_in" />
+      <Stack.Screen name="sign_up" />
+      <Stack.Screen name="reset_password" />
+    </Stack>
+  );
+}
